b246ca4db3e3bd45baffb3f386e283f5a0c184bc
jy <seven77770@qq.com>
2016-02-04 17:11:04 +0800

    测试url，返回response

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,19 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest 
+
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
-	"""docstring for SmokeTest"""
-	
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1,3)
+class HomePageTest(TestCase):
+
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
+
+	def test_home_page_returns_correct_html(self):
+		request = HttpRequest()
+		responese = home_page(request)
+		self.assertTrue(responese.content.startswith(b'<html>'))
+		self.assertIn(b'<title>To-Do lists</title>',responese.content)
+		self.assertTrue(responese.content.endswith(b'</html>'))
